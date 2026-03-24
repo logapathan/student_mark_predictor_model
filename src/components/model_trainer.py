7ed665b24c98aaf5f8709cfe2fe9013fca6f05c6
logapathan <logapathan@gmail.com>
2026-03-24 12:10:27 +0530

    hyperparameter tuning done

--- src/components/model_trainer.py
+++ src/components/model_trainer.py
@@ -44,7 +44,35 @@ class ModelTrainer:
                 "CatBoosting Regressor": CatBoostRegressor(verbose=False)
             }
 
-            model_report: dict = evaluate_models(x_Train, y_Train, x_Test, y_Test, models)
+            params={
+                "Decision Tree": {
+                    'criterion':['squared_error', 'friedman_mse', 'absolute_error', 'poisson']
+                },
+                "Random Forest":{
+                    'n_estimators': [8,16,32,64,128,256]
+                },
+                "Gradient Boosting":{
+                    'learning_rate':[.1,.01,.05,.001],
+                    'subsample':[0.6,0.7,0.75,0.8,0.85,0.9],
+                    'n_estimators': [8,16,32,64,128,256]
+                },
+                "Linear Regression":{},
+                "K-Neighbors Regressor":{
+                    'n_neighbors':[5,7,9,11],
+                    'weights':['uniform','distance']
+                },
+                "XGBRegressor":{
+                    'learning_rate':[.1,.01,.05,.001],
+                    'n_estimators': [8,16,32,64,128,256]
+                },
+                "CatBoosting Regressor":{
+                    'depth': [6,8,10],
+                    'learning_rate': [0.01, 0.05, 0.1],
+                    'iterations': [30, 50, 100]
+                }
+            }
+
+            model_report: dict = evaluate_models(x_Train, y_Train, x_Test, y_Test, models,params=params)
 
             best_model_score = max(model_report.values())
             best_model_name = [name for name, score in model_report.items() if score == best_model_score][0]
--- src/utils.py
+++ src/utils.py
@@ -2,6 +2,7 @@ import os
 import sys
 
 from sklearn.metrics import r2_score
+from sklearn.model_selection import GridSearchCV
 import dill
 import numpy as np
 import src.exceptions as CustomException
@@ -19,11 +20,17 @@ def save_object(file_path: str, obj: object) -> None:
         logging.error(f"Error occurred while saving object: {e}")
         raise CustomException(e, sys)
     
-def evaluate_models(x_train, y_train, x_test, y_test, models: dict) -> dict:
+def evaluate_models(x_train, y_train, x_test, y_test, models: dict,params) -> dict:
     try:
         report = {}
         for i in range(len(models)):
             model = list(models.values())[i]
+            params_for_model = params[list(models.keys())[i]]
+
+            gs= GridSearchCV(model,params_for_model,cv=5)
+            gs.fit(x_train,y_train)
+
+            model.set_params(**gs.best_params_)                  
             model.fit(x_train, y_train)
 
             y_test_pred = model.predict(x_test)
